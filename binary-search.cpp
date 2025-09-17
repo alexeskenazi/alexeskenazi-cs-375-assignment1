d53d8398ff5521527402792da0a9fdb679f9af4d
Alex Eskenazi <aeskena1@binghamton.edu>
2025-09-17 19:34:25 -0400

    Adding comment to binary search to make it easier to demo

--- binary-search.cpp
+++ binary-search.cpp
@@ -1,26 +1,40 @@
 
+
+
 #include "binary-search.h"
 
+// Operation counter
 long long binarySearchOperations = 0;
 
 int binarySearch(long long arr[], int size, long long target, long long *operationCount) {
     int left = 0, right = size - 1;
+
     long long localCount = 0;
     
     while (left <= right) {
+        // count this as one operation
         localCount++;
+        
+        // find the middle element
         int mid = left + (right - left) / 2;
         
+        // did we find it?
         if (arr[mid] == target) {
+            // yes! add our count and return where we found it
             *operationCount += localCount;
             return mid;
-        } else if (arr[mid] < target) {
+        } 
+        // the middle is too small, so look in the right half
+        else if (arr[mid] < target) {
             left = mid + 1;
-        } else {
+        } 
+        // the middle is too big, so look in the left half
+        else {
             right = mid - 1;
         }
     }
     
+    // we didn't find it
     *operationCount += localCount;
     return -1; 
 }
--- binary-search.h
+++ binary-search.h
@@ -1,10 +1,8 @@
 #ifndef BINARY_SEARCH_H
 #define BINARY_SEARCH_H
 
-// Function declarations for binary search
 int binarySearch(long long arr[], int size, long long target, long long *operationCount);
 
-// Global variable for operation counting
 extern long long binarySearchOperations;
 
 #endif
